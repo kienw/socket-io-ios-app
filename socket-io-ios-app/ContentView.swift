e00dfe42aedcc192df9a622de464e1c7fbe953c9
kien <kien@cimtops-sglab.com>
2024-08-19 21:14:09 +0800

    Initial Commit

---
+++ socket-io-ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  socket-io-ios-app
+//
+//  Created by Kien on 19/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ socket-io-ios-app/socket_io_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  socket_io_ios_appApp.swift
+//  socket-io-ios-app
+//
+//  Created by Kien on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct socket_io_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
